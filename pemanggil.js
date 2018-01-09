abac50d302626296c16098c5c4e1831665bc34fc
nurudinwafiq <wafiqnurudin@gmail.com>
2018-01-09 14:57:38 +0700

    Update pemanggil.js

--- pemanggil.js
+++ pemanggil.js
@@ -28,7 +28,7 @@ if(x=="4"){xxx="/2017/12/stay-legal-avoiding-insurance-fraud.html"}
 if(x=="5"){xxx="/2017/12/purchasing-brand-new-sports-car.html"}
 if(x=="6"){xxx="/2017/12/should-you-buy-motor-home-from-auction.html"}
 if(x=="7"){xxx="/2017/12/hamsters-fuzzy-balls-of-fun.html"}
-if(x=="8"){xxx="/2017/12/adsense-and-surfer.html"}a_to_ve[i].href="https://patihisafelink.blogspot.com"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
+if(x=="8"){xxx="/2018/01/tips-to-make-your-next-hiking-adventure.html"}a_to_ve[i].href="https://patihisafelink.blogspot.com"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
 var a_to_vj=document.getElementById("anonyminized");var a_to_vk=document.getElementById("found_links");if(a_to_vj)
 {a_to_vj.innerHTML+=a_to_vb;}
 if(a_to_vk)
